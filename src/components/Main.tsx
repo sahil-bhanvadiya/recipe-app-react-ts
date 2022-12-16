14abef7a1ae42faacc438a88ddc9f456c3f65252
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2022-12-16 17:15:48 +0530

    feat: homeWork completed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,48 +1,93 @@
-import { ChangeEvent, FC, useState } from "react"
+import { ChangeEvent, FC, useState } from "react";
 import { RecipeCard } from "./RecipeCard";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 interface Recipe {
-  id: number,
-  image : string,
-  name: string,
-  desc : string
+  id: number;
+  image: string;
+  name: string;
+  desc: string;
 }
 const recipesData: Recipe[] = [
   {
     id: 1,
-    image: "https://img.freepik.com/free-photo/aloo-paratha-gobi-paratha-also-known-as-potato-cauliflower-stuffed-flatbread-dish-originating-from-indian-subcontinent_466689-76186.jpg?size=626&ext=jpg",
-    name: 'Aalu Paratha',
-    desc: 'Aalu paratha is very good.'
+    image:
+      "https://img.freepik.com/free-photo/aloo-paratha-gobi-paratha-also-known-as-potato-cauliflower-stuffed-flatbread-dish-originating-from-indian-subcontinent_466689-76186.jpg?size=626&ext=jpg",
+    name: "Aalu Paratha",
+    desc: "Aalu paratha is very good recipe.",
   },
   {
     id: 2,
     image: "https://thumbs.dreamstime.com/b/bhel-puri-23902772.jpg",
-    name: 'Bhel',
-    desc: 'Bhelpuri is a savoury snack originating from India, and is also a type of chaat. It is made of puffed rice, vegetables and a tangy tamarind sauce, and has a crunchy texture.',
-  }
+    name: "Bhel",
+    desc: "Bhelpuri is a savoury snack originating from India, and is also a type of chaat. It is made of puffed rice, vegetables and a tangy tamarind sauce, and has a crunchy texture.",
+  },
+  {
+    id: 3,
+    image: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg",
+    name: "Pizza",
+    desc: "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese.",
+  },
 ];
 const Main: FC = () => {
-  const [state, setState] = useState<boolean>(false)
-  const [recipes, setRecipes] = useState<Recipe[]>(recipesData)
-  const [name, setName] = useState<string>('');
+  const [state, setState] = useState<boolean>(false);
+  const [recipes, setRecipes] = useState<Recipe[]>(recipesData);
+  const [name, setName] = useState<string>("");
+  const [img, setImg] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+
   const handleClose = () => {
-    console.log(name);
-    
+    console.log(name, img, desc);
     setState(false);
+  };
+
+  const getUniqueID = (): number => {
+    return +(Date.now() + (Math.random() * 100000).toFixed());
+  };
+
+  const clearForm = () => {
+    setName("");
+    setImg("");
+    setDesc("");
   }
+
+  const handleSave = () => {
+    const newRecipe: Recipe = {
+      id: getUniqueID(),
+      name,
+      image: img,
+      desc,
+    };
+    setRecipes((recipes) => [...recipes, newRecipe]);
+    clearForm();
+    setState(false);
+  };
+
   const handleShow = () => setState(true);
+
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-  } 
+    switch (e.target.name) {
+      case "name":
+        setName(e.target.value);
+        break;
+      case "img":
+        setImg(e.target.value);
+        break;
+      case "description":
+        setDesc(e.target.value);
+        break;
+    }
+  };
   return (
     <>
+    <div className="text-center mb-3">
       <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+        Add Recipe
       </Button>
+    </div>
       <Modal show={state} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add Recipe</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="mb-3">
@@ -80,15 +125,15 @@ const Main: FC = () => {
               className="form-control"
               id="exampleFormControlTextarea1"
               rows={3}
-              placeholder='Recipe Description.'
+              placeholder="Recipe Description."
             ></textarea>
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="danger" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="success" onClick={handleSave}>
             Save Changes
           </Button>
         </Modal.Footer>
@@ -102,6 +147,6 @@ const Main: FC = () => {
       </div>
     </>
   );
-}
+};
 
-export default Main
+export default Main;
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -8,9 +8,9 @@ export const RecipeCard = (props: any) => {
         <div className="card-body">
           <h5 className="card-title">{recipe.name}</h5>
           <p className="card-text">{recipe.desc}</p>
-          <a href="#" className="btn btn-primary">
+          {/* <a href="#" className="btn btn-primary">
             Download
-          </a>
+          </a> */}
         </div>
         <div className="card-footer">
             <small className="text-muted">Ingredients Here</small>
